ca4bfe187841cea3646a81c6a95decde80456b99
evillentenxon <romangautam71399@gmail.com>
2023-07-18 09:03:13 +0545

    inital commit

---
+++ src/Contact.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+
+    .container {
+      margin-top: 6rem;
+      text-align: center;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Feel Free to Contact us</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3565.653712883933!2d87.69190421001541!3d26.65956767082868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39e58f6bb2ae73a9%3A0x7da5eaee290218cc!2sCellPoint!5e0!3m2!1sen!2snp!4v1678959397735!5m2!1sen!2snp"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+              
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xgedgjkr"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              autoComplete="off"
+              required
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="6"
+              autoComplete="off"
+              required></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+import { NavLink } from "react-router-dom";
+import { FaDiscord, FaFacebook, FaFacebookF, FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <section className="contact-short">
+        <div className="grid grid-two-column">
+          <div>
+            <h3>Ready to order ?</h3>
+            <h3>We offer delivery service as well</h3>
+          </div>
+
+          <div className="contact-short-btn">
+            <NavLink to="/">
+              <Button>Get Started</Button>
+            </NavLink>
+          </div>
+        </div>
+      </section>
+
+      {/* footer section  */}
+
+      <footer>
+        <div className="container grid grid-four-column">
+          <div className="footer-about">
+            <h3>Cell-point</h3>
+            <p>Located in the Damak paschim bus stand side, on the opposite side of Gorkha Department Store.</p>
+          </div>
+
+          {/* 2nd column */}
+          <div className="footer-subscribe">
+            <h3>Follow our social media account for important and latest updates of mobile phones.</h3>
+            <form action="#">
+              <input
+                type="email"
+                required
+                autoComplete="off"
+                placeholder="Email"
+              />
+              <input type="submit" value="Buy Now" />
+            </form>
+          </div>
+
+          {/* 3rs column  */}
+          <div className="footer-social">
+            <h3>Follows Us</h3>
+            <div className="footer-social--icons">
+              <div>
+              <a
+                  href="https://www.facebook.com/profile.php?id=100084922516651&mibextid=ZbWKwL
+                  "
+                  target="_blank">                  
+                <FaFacebookF className="icons" />
+                </a>
+              </div>
+              <div>
+                <FaInstagram className="icons" />
+              </div>
+              <div>
+                <a
+                  href="https://www.tiktok.com/@cellpoint48?is_from_webapp=1&sender_device=pc"
+                  target="_blank">
+                  <FaTiktok className="icons" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* 4th column  */}
+          <div className="footer-contact">
+            <h3>Call Us</h3>
+            <h3>+977 981-7313776</h3>
+            <h3>+977 981-5000119</h3>
+          </div>
+        </div>
+
+        {/* bottom section  */}
+        <div className="footer-bottom--section">
+          <hr />
+          <div className="container grid grid-two-column">
+            <p>
+              @{new Date().getFullYear()} CellPoint. All Rights Reserved
+            </p>
+            <div>
+              <p>PRIVACY POLICY</p>
+              <p>TERMS & CONDITIONS</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
+  }
+  .contact-short-btn {
+    justify-self: end;
+    align-self: center;
+  }
+
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+
+    .footer-bottom--section {
+      padding-top: 9rem;
+
+      hr {
+        margin-bottom: 2rem;
+        color: ${({ theme }) => theme.colors.hr};
+        height: 0.1px;
+      }
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .contact-short {
+      max-width: 95vw;
+      padding: 2rem 0rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .contact-short-btn {
+        text-align: center;
+        justify-self: flex-start;
+      }
+    }
+
+    footer .footer-bottom--section {
+      padding-top: 3.2rem;
+    }
+  }
+`;
+
+export default Footer;
